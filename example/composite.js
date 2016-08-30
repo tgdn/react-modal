b343b6d31846b872f93cfd3782b44e83dbae039c
Thomas Gak Deluen <tgdn45@gmail.com>
2016-08-30 16:38:39 +0200

    Fixed indentation

--- example/composite.js
+++ example/composite.js
@@ -23,16 +23,16 @@ class ExampleComposite extends React.Component {
         return (
             <ModalComposite>
                 <button className='btn' href='#' onClick={this.showDialog.bind(this)}>open</button>
-                    <Modal
-                        visible
-                        ref={(c) => this.modal = c}>
-                        <ModalContent>
-                            This is an example
-                            <br />
-                            <br />
-                            <button className='btn' onClick={this.closeOnClick.bind(this)}>Close with a button</button>
-                        </ModalContent>
-                    </Modal>
+                <Modal
+                    visible
+                    ref={(c) => this.modal = c}>
+                    <ModalContent>
+                        This is an example
+                        <br />
+                        <br />
+                        <button className='btn' onClick={this.closeOnClick.bind(this)}>Close with a button</button>
+                    </ModalContent>
+                </Modal>
             </ModalComposite>
         )
     }
