0b7b37fb967ce9ce4f56a2e9c79bf9b3077029da
Thomas Gak Deluen <tgdn45@gmail.com>
2016-09-02 01:34:49 +0200

    Fixed npm package

--- webpack.config.js
+++ webpack.config.js
@@ -10,54 +10,60 @@ const stylesheets = fs.readdirSync(stylesheetFolder);
 const files = [];
 const entries = {};
 
-components.forEach(component => {
-  var name = component.split('.')[0];
-  var file = `./src/components/${name}`;
-  files.push(file);
-  entries[name] = file;
-});
-
-stylesheets.forEach(stylesheet => {
-  var name = stylesheet.split('.')[0];
-  var file = `./css/${name}`;
-  files.push(file);
-  entries[name] = file;
-});
+// components.forEach(component => {
+//     var name = component.split('.')[0];
+//     var file = `./src/components/${name}`;
+//     files.push(file);
+//     entries[name] = file;
+// });
+//
+// stylesheets.forEach(stylesheet => {
+//     var name = stylesheet;
+//     var file = `./css/${name}`;
+//     files.push(file);
+//     entries[name] = file;
+// });
 
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: entries,
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loaders: [ 'babel' ],
-        exclude: /node_modules/,
-        include: __dirname,
-        query: {
-            plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy', 'transform-object-rest-spread']
-        }
-      },
-      {
-        test: /\.css?$/,
-        loaders: [ 'style', 'raw' ],
-        include: __dirname
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        './src/index.js',
+        './css/index.sass',
+    ],
+    externals: {
+        'react': 'react',
+        'react-dom': 'react-dom'
     },
-      {
-        test:   /\.sass$/,
-        loaders: ['style', 'raw', 'sass'],
-        include: __dirname
-      },
-    ]
-  }
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        library: 'react-modal',
+        libraryTarget: 'umd',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel',
+            exclude: /node_modules/,
+            include: __dirname,
+            query: {
+                presets: ["es2015", "stage-0", "react"],
+                plugins: ["react-html-attrs", "transform-class-properties", "transform-decorators-legacy", "transform-object-rest-spread"]
+            }
+        }, {
+            test: /\.css?$/,
+            loaders: ['style', 'raw'],
+            include: __dirname
+        }, {
+            test: /\.sass$/,
+            loaders: ['style', 'raw', 'sass'],
+            include: __dirname
+        }, ]
+    }
 }
